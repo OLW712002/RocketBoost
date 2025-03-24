fb0e3329c91afb26abd5a62b1c720c7d5c7757dd
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-03-24 18:36:21 +0700

    Update Rocket

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] InputAction thrush;
+
+    private void OnEnable()
     {
-        
+        thrush.Enable();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (thrush.IsPressed()) Debug.Log("Im Flyiiiing");
     }
 }
