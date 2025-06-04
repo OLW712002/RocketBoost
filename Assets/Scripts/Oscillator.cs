ce58dd5d6638a2d5363ae4a89c66ca584cde3c75
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-06-04 10:12:30 +0700

    Optimize Moving logic

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Transform StartPosition;
-    [SerializeField] Transform EndPosition;
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Vector3 movementVector;
+
+    Vector3 StartPosition;
+    Vector3 EndPosition;
+    float movementFactor;
+    
 
     void Start()
     {
-        
+        StartPosition = transform.position;
+        EndPosition = transform.position + movementVector;
     }
 
     void Update()
     {
-        transform.position = Vector3.Lerp(StartPosition.position,EndPosition.position, Mathf.PingPong(Time.time * moveSpeed, 1));
+        movementFactor = Mathf.PingPong(Time.time * moveSpeed, 1);
+        transform.position = Vector3.Lerp(StartPosition, EndPosition, movementFactor);
     }
 }
